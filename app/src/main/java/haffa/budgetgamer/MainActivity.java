eb90fdb79f948fb848f091f7986b8a06dc01f553
Haffa <rafalkitab@gmail.com>
2016-12-10 16:29:40 +0100

    Added fab and sorting options

--- app/src/main/java/haffa/budgetgamer/MainActivity.java
+++ app/src/main/java/haffa/budgetgamer/MainActivity.java
@@ -4,24 +4,34 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
 import android.widget.Toast;
 
+import com.eminayar.panter.DialogType;
+import com.eminayar.panter.PanterDialog;
+import com.eminayar.panter.interfaces.OnSingleCallbackConfirmListener;
 import com.google.firebase.crash.FirebaseCrash;
 
 import haffa.budgetgamer.data.DataHandler;
 
 public class MainActivity extends AppCompatActivity {
     public final String BULK_DOWNLOAD_URL = "http://www.cheapshark.com/api/1.0/deals?";
+    public final String LOWEST_PRICE_URL = "http://www.cheapshark.com/api/1.0/deals?sortBy=Price";
+    public final String SAVINGS_PRICE_URL = "http://www.cheapshark.com/api/1.0/deals?sortBy=Savings";
+    public final String STEAM_URL = "http://www.cheapshark.com/api/1.0/deals?storeID=6";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         if (isOnline() == true) {
             DataHandler dataHandler = new DataHandler();
@@ -36,10 +46,21 @@ public class MainActivity extends AppCompatActivity {
         FirebaseCrash.report(new Exception("My first Android non-fatal error"));
 
         addFragments();
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-}
 
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                performDialogOperation();
+            }
+        });
+}
     public void addFragments(){
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -58,6 +79,48 @@ public class MainActivity extends AppCompatActivity {
                 (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo netInfo = cm.getActiveNetworkInfo();
         return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+    public void performDialogOperation(){
+
+        PanterDialog panterDialog = new PanterDialog(this);
+                panterDialog.setHeaderBackground(R.drawable.pattern_bg_orange)
+                .setHeaderLogo(R.drawable.logo)
+                .setDialogType(DialogType.SINGLECHOICE)
+                .isCancelable(false)
+                .items(R.array.sample_array, new OnSingleCallbackConfirmListener() {
+                    @Override
+                    public void onSingleCallbackConfirmed(PanterDialog dialog, int pos, String text) {
+                        DataHandler dataHandler = new DataHandler();
+                        switch(pos){
+                            case 0:
+                                Log.v("case 1", "case 1");
+                                dataHandler.downloadData(BULK_DOWNLOAD_URL);
+                                addFragments();
+                                break;
+                            case 1:
+                                Log.v("case 2", "case 2");
+                                dataHandler.downloadData(LOWEST_PRICE_URL);
+                                addFragments();
+                                break;
+                            case 2:
+                                Log.v("case 3", "case 3");
+                                dataHandler.downloadData(SAVINGS_PRICE_URL);
+                                addFragments();
+                                break;
+                            case 3:
+                                Log.v("case 4", "case 4");
+                                dataHandler.downloadData(STEAM_URL);
+                                addFragments();
+                                break;
+                            default:
+                                return;
+                        }
+
+
+                    }
+                });
+                panterDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                panterDialog.show();
     }
 
 }
